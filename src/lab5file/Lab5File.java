714875dfeecb799c814eab79c12271222e4c6c5a
JeanNajar <jean_carlos@unitec.edu>
2026-02-27 10:53:34 -0600

    avance gui 1.2

--- src/lab5file/Lab5File.java
+++ src/lab5file/Lab5File.java
@@ -1,11 +1,19 @@
 package lab5file;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Lab5File {
-
-
     public static void main(String[] args) {
-
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+        SwingUtilities.invokeLater(() -> {
+            SystemFile fs = new SystemFile();
+            cmdGUI gui = new cmdGUI();
+            Procesos processor = new Procesos(fs, gui);
+            gui.setProcessor(processor);
+            gui.setVisible(true);
+        });
     }
-
 }
